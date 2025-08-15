1a47c5175a79f45d86a85557ff0f690748d1684f
dezhishen <26274059+dezhishen@users.noreply.github.com>
2025-08-15 11:22:04 +0800

    test:增加性能测试

--- benchmark_test.go
+++ benchmark_test.go
@@ -5,6 +5,31 @@ import (
 	"testing"
 )
 
+func BenchmarkInit(b *testing.B) {
+	// compile the wasm files first if needed
+	drivers, err := LoadPlugins("./examples/wasm")
+	if err != nil {
+		b.Fatal(err)
+	}
+	// use the first driver for benchmarking
+	if len(drivers) == 0 {
+		b.Fatal("no drivers loaded")
+	}
+	driver := drivers[0]
+	// 强转为 WasmPluginDriver
+	wasmDriver, ok := driver.(*wasmPluginDriver)
+	if !ok {
+		b.Fatal("driver is not a WasmPluginDriver")
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		wasmDriver.Close() // 重置 runtime 以便下次测试
+		b.StartTimer()
+		wasmDriver.Init()
+	}
+}
+
 func BenchmarkCallWithInitPlugin(b *testing.B) {
 	// compile the wasm files first if needed
 	drivers, err := LoadPlugins("./examples/wasm")
@@ -26,7 +51,7 @@ func BenchmarkCallWithInitPlugin(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
-		wasmDriver.runtime = nil // 重置 runtime 以便下次测试
+		wasmDriver.Close() // 重置 runtime 以便下次测试
 		b.StartTimer()
 		wasmDriver.Name(ctx)
 	}
@@ -42,18 +67,17 @@ func BenchmarkCallWithOutInitPlugin(b *testing.B) {
 		b.Fatal("no drivers loaded")
 	}
 	driver := drivers[0]
-	ctx := context.Background()
 	wasmDriver, ok := driver.(*wasmPluginDriver)
 	if !ok {
 		b.Fatal("driver is not a WasmPluginDriver")
 	}
-	wasmDriver.Init(ctx)
+	ctx := context.Background()
+	wasmDriver.Init()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		name := wasmDriver.Name(ctx)
 		if name != "unimplemented" {
 			b.Fatal("driver name is empty")
 		}
-		ctx.Done()
 	}
 }
